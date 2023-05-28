6bcc69eceff17152f447abe61df4696db81e3178
Kris Ivan <krisztian.ivan@gmail.com>
2023-05-28 11:55:41 +0100

    added implementation for finding slots for employee with no serviceId passed

---
+++ AppointmentBooking.Domain.Tests.Unit/SlotsServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using AppointmentBooking.Domain.Services;
+using AppointmentBooking.Domain.Tests.Unit.Fakes;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+
+namespace AppointmentBooking.Domain.Tests.Unit
+{
+    public class SlotsServiceTests : IDisposable
+    {
+        private readonly ApplicationContextFakeBuilder _contextBuilder = new();
+        private readonly INowService _nowService = Substitute.For<INowService>();
+        private readonly ApplicationSettings _applicationSettings = new()
+        { OpenAppointmentInDays = 7, RoundUpInMin = 5, RestInMin = 5 };
+        private readonly IOptions<ApplicationSettings> _settings = Substitute.For<IOptions<ApplicationSettings>>();
+        private SlotsService? _sut;
+
+        public SlotsServiceTests()
+        {
+            _settings.Value.Returns(_applicationSettings);
+        }
+
+        public void Dispose()
+        {
+            _contextBuilder.Dispose();
+        }
+
+        [Fact]
+        public async Task GetAvailableSlotsForEmployee_ServiceIdNotFound_ArgumentException()
+        {
+            // Arrange
+            var ctx = _contextBuilder.Build();
+            _sut = new SlotsService(ctx, _nowService, _settings);
+
+            // Act
+            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetAvailableSlotsForEmployee(-1, -1));
+
+            // Assert
+            Assert.IsType<ArgumentException>(exception);
+        }
+    }
+}
--- AppointmentBooking.Domain/Services/SlotsService.cs
+++ AppointmentBooking.Domain/Services/SlotsService.cs
@@ -1,4 +1,7 @@
-﻿using AppointmentBooking.Domain.Report;
+﻿using AppointmentBooking.Domain.Database;
+using AppointmentBooking.Domain.Report;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace AppointmentBooking.Domain.Services
 {
@@ -11,14 +14,38 @@ namespace AppointmentBooking.Domain.Services
 
     public class SlotsService : ISlotsService
     {
+        private readonly ApplicationContext _context;
+        private readonly ApplicationSettings _settings;
+        private readonly DateTime _now;
+        private readonly TimeSpan _roundingIntervalSpan;
+
+        public SlotsService(ApplicationContext context, INowService nowService, IOptions<ApplicationSettings> settings)
+        {
+            _context = context;
+            _settings = settings.Value;
+            _roundingIntervalSpan = TimeSpan.FromMinutes(_settings.RoundUpInMin);
+            _now = RoundUpToNearest(nowService.Now);
+        }
+
         public async Task<Slots> GetAvailableSlotsForEmployee(int serviceId, int employeeId)
         {
-            throw new NotImplementedException();
+            var service = await _context.Services!.SingleOrDefaultAsync(x => x.Id == serviceId);
+            if (service is null)
+            {
+                throw new ArgumentException($"Service with id {serviceId} not found");
+            }
+            return null;
         }
 
         public async Task<Slots> GetCombinedAvailableSlots(int serviceId)
         {
             throw new NotImplementedException();
+        }
+
+        private DateTime RoundUpToNearest(DateTime dt)
+        {
+            var ticksInSpan = _roundingIntervalSpan.Ticks;
+            return new DateTime((dt.Ticks + ticksInSpan - 1) / ticksInSpan * ticksInSpan, dt.Kind);
         }
     }
 }
